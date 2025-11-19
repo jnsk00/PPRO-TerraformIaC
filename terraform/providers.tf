4153e3760676c685e642db84b0c5b827a2484a16
jnsk00 <100359273+jnsk00@users.noreply.github.com>
2025-11-19 18:10:10 +0100

    Add Terraform providers configuration for Docker and Kubernetes

---
+++ terraform/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.27.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+provider "kubernetes" {}
